ae5c462d535bfd16d61afb13d5cbf38c7913e1bc
clogboy <bweinreder@gmail.com>
2026-03-17 11:36:43 +0000

    model initiation modified

--- main.py
+++ main.py
@@ -2,32 +2,18 @@ import sys
 
 from PyQt6.QtWidgets import QApplication
 
-from core.ifc_loader import IfcLoader
-from core.model_index import ModelIndex
-from core.sql_store import SQLStore
-
 from ui.main_window import MainWindow
 
 def main():
 
-    if len(sys.argv) < 2:
-        print("Usage: python main.py model.ifc")
-        return
+    path = ""
 
-    path = sys.argv[1]
-
-    loader = IfcLoader()
-    model = loader.load(path)
-
-    index = ModelIndex()
-    index.build(model)
-
-    db = SQLStore("model.db")
-    db.store(index)
+    if not len(sys.argv) < 2:
+        path = sys.argv[1]
 
     app = QApplication(sys.argv)
 
-    window = MainWindow(index)
+    window = MainWindow(path)
     window.show()
 
     sys.exit(app.exec())
--- ui/main_window.py
+++ ui/main_window.py
@@ -1,16 +1,23 @@
 from PyQt6.QtWidgets import QMainWindow, QSplitter, QTreeWidget, QTreeWidgetItem
 from PyQt6.QtCore import Qt
 
+from core.ifc_loader import IfcLoader
+from core.model_index import ModelIndex
+from core.sql_store import SQLStore
+
 from ui.vtk_viewer import VTKViewer
 
 
 class MainWindow(QMainWindow):
 
-    def __init__(self, index):
+    def __init__(self, path):
 
         super().__init__()
 
-        self.index = index
+        self.path = path
+
+        if not path = "":
+            initModel(path)
 
         self.setWindowTitle("IFC File Companion")
         self.resize(1400, 900)
@@ -19,6 +26,23 @@ class MainWindow(QMainWindow):
 
         self.tree = QTreeWidget()
         self.tree.setHeaderLabels(["Element", "Type"])
+
+        splitter.addWidget(self.tree)
+        splitter.addWidget(self.viewer)
+
+        splitter.setSizes([350, 1050])
+
+        self.setCentralWidget(splitter)
+    
+    def initModel(path):
+        loader = IfcLoader()
+        model = loader.load(path)
+
+        index = ModelIndex()
+        index.build(model)
+
+        db = SQLStore("model.db")
+        db.store(index)
 
         for element in self.index.elements:
 
@@ -33,9 +57,3 @@ class MainWindow(QMainWindow):
 
         self.viewer = VTKViewer()
 
-        splitter.addWidget(self.tree)
-        splitter.addWidget(self.viewer)
-
-        splitter.setSizes([350, 1050])
-
-        self.setCentralWidget(splitter)
